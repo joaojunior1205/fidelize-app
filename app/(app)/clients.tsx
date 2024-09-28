07fac71a0bbc4c307faee27d4c112cf6235498a1
Joao Junior <joaojunior1205@gmail.com>
2024-09-28 00:39:46 -0300

    feat: list clients

--- app/(app)/clients.tsx
+++ app/(app)/clients.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import {
     View,
     Text,
+    FlatList,
+    StyleSheet,
 } from 'react-native';
 import ClientApi from "@/api/auth/client-api";
+import colors from "../colors";
 
 export default function ClientsScreen() {
     const [loading, setLoading] = React.useState(true);
@@ -24,8 +27,48 @@ export default function ClientsScreen() {
     }
 
     return (
-        <View>
-            <Text>{JSON.stringify(client)}</Text>
+        <View style={styles.container}>
+            <FlatList
+                data={client}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item }) => (
+                    <View style={styles.cardContainer}>
+                        <Text style={styles.clientName}>
+                            {item.name || '-'}
+                        </Text>
+
+                        <Text style={styles.phoneNumber}>
+                            {item?.phone || '-'}
+                        </Text>
+                    </View>
+                )}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+
+    cardContainer: {
+        borderBottomWidth: 1,
+        borderColor: colors.lineBorderColor,
+        padding: 7,
+        gap: 4
+    },
+
+    clientName: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: colors.textColor
+    },
+
+    phoneNumber: {
+        fontSize: 12,
+        fontWeight: '400',
+        color: colors.textColor,
+    }
+})
---
+++ app/colors.ts
@@ -0,0 +1,4 @@
+export default {
+    lineBorderColor: '#d0d0d0',
+    textColor: '#1a1a1a'
+};
